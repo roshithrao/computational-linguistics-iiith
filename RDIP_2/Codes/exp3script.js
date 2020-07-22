542d5398d76ae508ffe4a99ed3bd630a781872b2
roshithrao <67267016+roshithrao@users.noreply.github.com>
2020-07-22 15:46:55 +0530

    Defined Functionality of Submit Button[UNIT-5]

--- RDIP_2/Codes/exp3script.js
+++ RDIP_2/Codes/exp3script.js
@@ -8,15 +8,18 @@ var hindi_sentences=['राम ने सीता के लिए फल त�
 var sentence = " ";
 var value1 = " ";
 var user_pos_values = [];
+var english_actual_ans_values = [];
+var hindi_actual_ans_values = [];
+var j = 0;
 
- var docx = nlp("Roshith rao am so cool");
+ //var docx = nlp("bravest");
  //console.log(docx.data());
 
- console.log(docx.sentences().terms().out('array'));
+ //console.log(docx.sentences().terms().out('array'));
 
- console.log(docx.sentences().terms().out('tags'));
+ //console.log(docx.sentences().out('tags'));
 
- console.log(docx.nouns().terms().text())
+ //console.log(docx.nouns().terms().text())
 
  function expdisp(value)
  {
@@ -30,6 +33,7 @@ var user_pos_values = [];
     {
     	 
           value1 = value;
+          j =0;
           document.getElementById("message").innerHTML = " ";
           document.getElementById("table-disp").innerHTML = " ";
           document.getElementById("submit-button").style.display = "none";
@@ -41,6 +45,7 @@ var user_pos_values = [];
     {       
     	   
            value1 = value;
+           j = 0;
     	   document.getElementById("message").innerHTML = " ";
            document.getElementById("table-disp").innerHTML = " "; 
            document.getElementById("submit-button").style.display = "none";
@@ -65,29 +70,34 @@ var user_pos_values = [];
 	  else if(value === "Esen1")
 	  {
 		sentence = english_sentences[0];
+		j = 0;
 	  }	
 	  else if(value === "Esen2")
 	  {	
 	  	sentence = english_sentences[1];
+	  	j = 1;
 	  }	
       else if(value === "Esen3")
 	  {
 		sentence = english_sentences[2];
+		j = 2;
 	  }	
 	  else if(value === "Esen4")
 	  {
 	  	sentence = english_sentences[3];
+	  	j = 3;
 	  }	
       else if(value === "Esen5")
 	  {
 		sentence = english_sentences[4];
+		j = 4;
 	  }	
       
       sentence= sentence.replace("."," ");
       sentence=sentence.split(" ");
-	  var col="<tr id='tablehead' style='color:brown'><td>LEXICON</td><td>POS</td><td></td><td></td><td></td></tr>";
+	  var col="<tr id='tablehead' style='color:brown;font-size:20px'><td>LEXICON</td><td>POS</td><td></td><td></td></tr>";
 	  for(var i = 0; i < sentence.length; i++){
-		col = col +"<tr id='id"+i+"'><td>"+sentence[i]+"</td><td><select  id='posopt'><option value='Noun'>Noun</option><option value='Pronoun'>Pronoun</option><option value='Verb'>Verb</option><option value2='Adjective'>Adjective</option><option  value2 = 'Adverb'>Adverb</option><option value2='Determiner'>Determiner</option><option value2='Preposition'>Preposition</option><option value2='Conjunction'>Conjunction</option><option value2='Interjection'>Interjection</option></select></td><td></td><td></td><td></td></tr>";
+		col = col +"<tr id='id"+i+"' style='font-size:18px;color:black;'><td>"+sentence[i]+"</td><td><select  id='posoption"+i+"' onchange='user_ans_array(this.id,this.value)'><option value='Noun'>Noun</option><option value='Pronoun'>Pronoun</option><option value='Verb'>Verb</option><option value='Adjective'>Adjective</option><option  value = 'Adverb'>Adverb</option><option value='Determiner'>Determiner</option><option value='Preposition'>Preposition</option><option value='Conjunction'>Conjunction</option><option value='Interjection'>Interjection</option></select></td><td id='img"+i+"'></td><td></td></tr>";
 	   user_pos_values[i] = "Noun"; //setting default user pos values as Noun
 	 }
      
@@ -106,29 +116,34 @@ var user_pos_values = [];
 	  if(value === "Hsen1")
 	  {
 		sentence = hindi_sentences[0];
+		j = 0;
 	  }	
 	  else if(value === "Hsen2")
 	  {	
 	  	sentence = hindi_sentences[1];
+	  	j =1;
 	  }	
       if(value === "Hsen3")
 	  {
 		sentence = hindi_sentences[2];
+		j = 2;
 	  }	
 	  else if(value === "Hsen4")
 	  {
 	  	sentence = hindi_sentences[3];
+	  	j=3;
 	  }	
       if(value === "Hsen5")
 	  {
 		sentence = hindi_sentences[4];
+		j = 4;
 	  }	
       
       sentence= sentence.replace("."," ");
       sentence=sentence.split(" ");
-	  var col="<tr id= 'tablehead' style='color:brown'><td>LEXICON</td><td>POS</td><td></td><td></td><td></td></tr>";
+	  var col="<tr id= 'tablehead' style='color:brown;font-size:20px'><td>LEXICON</td><td>POS</td><td></td><td></td></tr>";
 	  for(var i = 0; i < sentence.length; i++){
-		col = col +"<tr id='id"+i+"'><td>"+sentence[i]+"</td><td><select  id='posopt'><option value = 'Noun'>Noun</option><option value='Pronoun'>Pronoun</option><option value='Verb'>Verb</option><option value='Adjective'>Adjective</option><option  value = 'Adverb'>Adverb</option><option value='Determiner'>Determiner</option><option value='Postposition'>Postposition</option><option value='Conjunction'>Conjunction</option><option value='Interjection'>Interjection</option></select></td><td></td><td></td><td></td></tr>";
+		col = col +"<tr id='id"+i+"' style='font-size:18px;color:black;'><td>"+sentence[i]+"</td><td><select  id='posoption"+i+"' onchange='user_ans_array(this.id,this.value)'><option value = 'Noun'>Noun</option><option value='Pronoun'>Pronoun</option><option value='Verb'>Verb</option><option value='Adjective'>Adjective</option><option  value = 'Adverb'>Adverb</option><option value='Determiner'>Determiner</option><option value='Postposition'>Postposition</option><option value='Conjunction'>Conjunction</option><option value='Interjection'>Interjection</option></select></td><td id='img"+i+"'></td><td></td></tr>";
 	  
 	   user_pos_values[i] = "Noun"; //setting default user pos values as Noun
 
@@ -136,9 +151,144 @@ var user_pos_values = [];
      
       document.getElementById("table-disp").innerHTML = col;
       document.getElementById("submit-button").style.display ="inline"; //displaying submit button below table
-
+     
 
  	}
  	
-    console.log(user_pos_values);
+    //console.log(user_pos_values);
+    document.getElementById("submit-button").onclick = function(){ compare_user_and_ans_pos()};
  }
+
+
+ function user_ans_array(id,value)
+ {
+    switch(id)
+    {
+       case 'posoption0' : user_pos_values[0] = value; 
+                           break;
+       case 'posoption1' : user_pos_values[1] = value;
+                           break;
+       case 'posoption2' : user_pos_values[2] = value;
+                           break;
+       case 'posoption3' : user_pos_values[3] = value;
+                           break;
+       case 'posoption4' : user_pos_values[4] = value;
+                           break;
+       case 'posoption5' : user_pos_values[5] = value;
+                           break;
+       case 'posoption6' : user_pos_values[6] = value;
+    }
+
+ }
+
+function compare_user_and_ans_pos()
+{
+
+   //console.log(user_pos_values);
+   if(value1 == "English")
+   {
+        for(var i =0;i<sentence.length;i++)
+       {
+           var docx = nlp(sentence[i]);
+           var noun_word = docx.nouns().text();
+           var pronoun_word = docx.pronouns().text();
+           var verb_word  = docx.verbs().text();
+           var adjective_word = docx.adjectives().text();
+           var adverb_word = docx.adverbs().text();
+           var preposition_word = docx.prepositions().text();
+           var conjunction_word = docx.conjunctions().text();
+
+
+		if(noun_word!="")
+		{
+			english_actual_ans_values[i]="Noun";
+		}
+		else if(pronoun_word!="")
+		{
+			english_actual_ans_values[i]="Pronoun";
+		}
+		else if(verb_word!="")
+		{
+			english_actual_ans_values[i]="Verb";
+		}
+		else if(adjective_word!="")
+		{	
+			english_actual_ans_values[i]="Adjective";
+		}
+		else if(adverb_word!="")
+		{	
+			english_actual_ans_values[i]="Adverb";
+		}	
+		else if(preposition_word!="")
+		{	
+			english_actual_ans_values[i]="Preposition";
+		}	
+		else if(conjunction_word!="")
+		{	
+			english_actual_ans_values[i]="Conjunction";
+		}	
+		else
+		{	
+			english_actual_ans_values[i]="Determiner";
+        } 
+          if(sentence[i]==="bravest")
+          {
+             english_actual_ans_values[i]="Adjective";
+          }
+          if(sentence[i]==="dress")
+          {
+        	 english_actual_ans_values[i] = "Noun";
+          }
+
+        
+      }
+
+        console.log(english_actual_ans_values);
+        for(var i=0;i<sentence.length;i++)
+        {
+        	var image = " ";
+           if(user_pos_values[i]==english_actual_ans_values[i])
+           {
+               image = "img"+i;
+               document.getElementById(image).innerHTML='<img src="http://cl-iiith.vlabs.ac.in/exp7/right.png" width="50px" height="50px">';
+
+           }
+           else
+           {
+               image = "img"+i;
+               document.getElementById(image).innerHTML='<img src="http://cl-iiith.vlabs.ac.in/exp7/wrong.png" width="50px" height="50px">';        
+
+                  
+           }
+
+        }
+
+   }
+   else if(value1 == "Hindi")
+   {
+       
+       hindi_actual_ans_values = [["Noun","Postposition","Noun","Postposition","Postposition","Noun","Verb"],["Adjective","Noun","Noun","Adverb","Verb"],["Noun","Postposition","Noun","Adjective","Verb","Verb"],["Interjection","Pronoun","Adjective","Verb"],["Noun","Postposition","Noun","Verb","Verb"]];
+
+      for(var i =0;i<sentence.length;i++)
+      {
+           var image = " "
+       if(user_pos_values[i]==hindi_actual_ans_values[j][i])
+           {
+               image = "img"+i;
+               console.log(image);
+               document.getElementById(image).innerHTML='<img src="http://cl-iiith.vlabs.ac.in/exp7/right.png" width="30px" height="30px">';
+
+           }
+           else
+           {
+               var image = "img"+i;
+               console.log(image);
+               document.getElementById(image).innerHTML='<img src="http://cl-iiith.vlabs.ac.in/exp7/wrong.png" width="30px" height="30px">';        
+           }
+      } 
+  }
+
+
+
+
+} 
